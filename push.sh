b6ee110d27b0c9e4063205f756b8aa2d20f07083
haoyulong <haoyulong@haotang365.com.cn>
2018-08-09 18:20:46 +0800

    update 2018-08-09

--- push.sh
+++ push.sh
@@ -23,7 +23,7 @@ git commit -m'update '${now}
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git pull origin master
+# git pull origin master
 git push -u origin master
 
 cd -
